2dab10a3afc48131714fa62e60f858ecf030ceb3
bobby-stanoff <hongnguyen210180@gmail.com>
2024-06-06 17:39:10 +0700

    first

---
+++ final/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'http://localhost:5000/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '<YOUR_KEY>';
+export const MODAL_CLOSE_SEC = 2.5;
